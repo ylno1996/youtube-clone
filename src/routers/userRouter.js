d270ea2280d20090119511caf147b2b00b76d417
ylno1996 <86780555+ylno1996@users.noreply.github.com>
2022-02-10 18:20:25 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 userRouter.get("/logout", logOut);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", deleteU);
-userRouter.get("/:id", see);
+userRouter.get("/:id(\\d+)", see);
 
 export default userRouter;
