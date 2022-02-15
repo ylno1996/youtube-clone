7054c64260743da2ed8cf62925ac2aca446fc286
ylno1996 <86780555+ylno1996@users.noreply.github.com>
2022-02-15 18:20:14 +0900

    data 0.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,45 +1,13 @@
-const fakeUser = {
-    username: "현우",
-    loggedIn: false,
+import Video from "../models/Video";
+
+export const homeVideos = (req, res) => {
+Video.find({}, (error,videos) => {
+    console.log(error, videos)
+})
+    res.render("home", {pageTitle: "집!"})
 };
 
-let videos = [
-    {
-        title: "1st Video",
-        rating: 7,
-        comments: 15,
-        createdAt: "2011-03-12",
-        views: 12321,
-        id: 1 
-    },
-    {
-        title: "2nd Video",
-        rating: 2,
-        comments: 315,
-        createdAt: "2021-05-23",
-        views: 131,
-        id: 2 
-    },
-    {
-        title: "3rd Video",
-        rating: 6,
-        comments: 1143165,
-        createdAt: "2001-01-18",
-        views: 12321,
-        id: 3 
-    },
-    {
-        title: "4th Video",
-        rating: 10,
-        comments: 15231,
-        createdAt: "2051-03-12",
-        views: 12312321,
-        id: 4 
-    },
-];
 
-
-export const homeVideos = (req, res) => res.render("home", {pageTitle: "집!", fakeUser, videos});
 export const watch = (req, res) => {
     const id = req.params.id;
     const video = videos[id-1];
@@ -64,14 +32,5 @@ res.render("upload", {pageTitle: "비디오 업로드",fakeUser})
 
 export const postUpload = (req, res) => {
     const title = req.body.title;
-    const newVideo = {
-        title, 
-        rating: 3,
-        comments: 5,
-        createdAt: "2005-12-01",
-        views: 55981,
-        id: videos.length +1,
-    };
-    videos.push(newVideo);
 res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -7,7 +6,6 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -24,9 +22,4 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
-
-console.log("테스트");
+export default app;
