c8cf151c0592c41a267fd77639bf8351255d9a5a
ylno1996 <86780555+ylno1996@users.noreply.github.com>
2022-02-25 18:22:26 +0900

    10? ah...

--- src/init.js
+++ src/init.js
@@ -9,6 +9,3 @@ const PORT = 4000;
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
-
-
-//Something going wrong now!!! need to fix it :(
